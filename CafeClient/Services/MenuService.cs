911de440c06ade1e294e3f26f8bed37298485b66
Tejus <tejusmdn@gmail.com>
2023-02-13 22:16:08 +1000

    Updated the MenuURL

--- CafeClient/Services/MenuService.cs
+++ CafeClient/Services/MenuService.cs
@@ -5,15 +5,19 @@ namespace CafeClient.Services
     public class MenuService : IMenuService
     {
         private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
 
-        public MenuService(HttpClient httpClient)
+        public MenuService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
         }
 
         public async Task<List<Item>> GetMenuItems()
         {
-            var responseMessage = await _httpClient.GetStringAsync("http://localhost:7015/api/Menu");
+            var menuUrl = _configuration["menuUrl"];
+
+            var responseMessage = await _httpClient.GetStringAsync(menuUrl);
             var items = JsonConvert.DeserializeObject<IEnumerable<Item>>(responseMessage);
             return items?.ToList() ?? new List<Item>();
         }
